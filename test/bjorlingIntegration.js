b7f44f4bfc347da37eb896b04a3f2f54366e569a
bmavity <brian@brianmavity.com>
2013-10-11 10:15:50 -0500

    Integration testing for full bjorling stack

--- test/bjorlingIntegration.js
+++ test/bjorlingIntegration.js
@@ -4,7 +4,7 @@ var storage = require('../')
 	, eb = require('./eb')
 	, leveldown = require('leveldown')
 
-describe('bjorling storage integration, when retrieving projection state', function() {
+describe('bjorling storage integration, when retrieving projection state by id', function() {
 	var db
 		, state
 
@@ -76,10 +76,107 @@ describe('bjorling storage integration, when retrieving projection state', funct
   })
 
   it('should contain the first event id', function() {
-  	state.ids.should.contain(3)
+  	state.ids.should.include(3)
   })
 
   it('should contain the second event id', function() {
-  	state.ids.should.contain(1)
+  	state.ids.should.include(1)
+  })
+})
+
+describe('bjorling storage integration, when retrieving projection state by an index', function() {
+	var db
+		, state
+
+	before(function(done) {
+		var s = storage(dbPath)
+			, b = bjorling(__filename, {
+					storage: s
+				, key: 'storageId'
+				})
+		db = s._db
+		b.addIndex('lockerId')
+		b.when({
+			'$new': function(e) {
+				return {
+					storageId: e.data.storageId
+				, count: 0
+				, ids: []
+				}
+			}
+		, 'FirstEvent': function(s, e) {
+				s.count += 1
+				s.ids.push(e.eventId)
+			}
+		, 'SecondEvent': function(s, e) {
+				s.count += 2
+				s.lockerId = e.lockerId
+				s.ids.push(e.eventId)
+			}
+		, 'ThirdEvent': function(s, e) {
+				s.count += 3
+				s.ids = []
+			}
+		})
+
+		function getEntry() {
+			b.get({ lockerId: 178 }, function(err, val) {
+				if(err) return done(err)
+				state = val
+				done()
+			})
+		}
+
+		function addEvent(evt, cb) {
+			b.processEvent(evt, cb)
+		}
+
+		setImmediate(function() {
+			var e1 = {
+						__type: 'FirstEvent'
+					, data: {
+							storageId: 'abcdef'
+						, eventId: 3
+						}
+					}
+				, e2 = {
+						__type: 'SecondEvent'
+					, data: {
+							storageId: 'abcdef'
+						, lockerId: 178
+						, eventId: 1
+						}
+					}
+				, e3 = {
+						__type: 'ThirdEvent'
+					, data: {
+							lockerId: 178
+						}
+					}
+			addEvent(e1, function() {
+				addEvent(e2, function() {
+					addEvent(e3, getEntry)
+				})
+			})
+		})
+	})
+
+	after(function(done) {
+		db.close(function(err) {
+			if(err) done()
+			leveldown.destroy(dbPath, done)
+		})
+	})
+
+  it('should have the proper count', function() {
+  	state.count.should.equal(6)
+  })
+
+  it('should not contain the first event id', function() {
+  	state.ids.should.not.include(3)
+  })
+
+  it('should contain the second event id', function() {
+  	state.ids.should.not.include(1)
   })
 })
